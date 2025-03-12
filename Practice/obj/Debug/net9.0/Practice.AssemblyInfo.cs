831e1ee116219de2ab127a281385b6135f96fb6e
Abdisalam M Yusuf <ayusufm.dev@outlook.com>
2025-03-12 19:39:25 +0300

    work

--- Practice/obj/Debug/net9.0/Practice.AssemblyInfo.cs
+++ Practice/obj/Debug/net9.0/Practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12d7ddffb5c035b62128a31ea6af74b9126c2ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38573ae888bbf4bd1fb07fe0d1205d95aed5d7e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
